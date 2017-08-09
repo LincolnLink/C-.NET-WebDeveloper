6c6d0d116023c97041b6177cda0825152a12cce7
Lincoln Ferreira Campos <link7-7@hotmail.com>
2017-08-08 21:54:54 -0300

    Desenvolvimento de diretorios e Entidades

---
+++ Projeto08/Projeto.Entidades/Estoque.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entidades
+{
+    public class Estoque
+    {
+        #region Atributo
+        private int idEstoque;
+        private string nome;
+        private string descricao;
+        private List<Produto> produtos; //lista
+        #endregion
+
+        #region Contrutores
+        public Estoque()
+        {
+
+        }
+        public Estoque(int idEstoque, string Nome, string descricao)
+        {
+            IdEstoque = idEstoque;
+            Nome = nome;
+            Descricao = descricao;
+        }
+
+        #endregion
+
+        #region Propriedade
+        public int IdEstoque
+        {
+            set { idEstoque = value; }
+            get { return idEstoque; }
+        }
+        public string Nome
+        {
+            set { nome = value; }
+            get { return nome; }
+        }
+        public string Descricao
+        {
+            set { descricao = value; }
+            get { return descricao; }
+        }
+        public List<Produto> Produtos
+        {
+            set { produtos = value; }
+            get { return produtos; }
+        }
+        #endregion
+    }
+}
---
+++ Projeto08/Projeto.Entidades/Produto.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entidades
+{
+    public class Produto
+    {
+        #region Atributos
+        private int idProduto;
+        private string nome;
+        private decimal preco;
+        private int quantidade;
+        private DateTime dataCadastro;
+        private Estoque estoque;
+        #endregion
+
+        #region Contrutor
+        public Produto()
+        {
+
+        }
+        public Produto(int idProduto, string nome, decimal preco, int quantidade, DateTime dataCadastro)
+        {
+            IdProduto = idProduto;
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+            DataCadastro = dataCadastro;
+        }
+        #endregion
+
+        #region Propriedade       
+        public int IdProduto
+        {
+            set {idProduto = value; }
+            get { return idProduto; }
+        }
+        public string Nome
+        {
+            set { nome = value; }
+            get { return nome; }
+        }
+        public decimal Preco
+        {
+            set { preco = value; }
+            get { return preco; }
+        }
+        public int Quantidade
+        {
+            set { quantidade = value; }
+            get { return quantidade; }
+        }
+        public DateTime DataCadastro
+        {
+            set { dataCadastro = value; }
+            get { return dataCadastro; }
+        }
+        public Estoque Estoque
+        {
+            set { estoque = value; }
+            get { return estoque; }
+        }
+        #endregion
+
+        #region Sobreescrita
+        public override string ToString()
+        {
+            return "Id: " + idProduto +
+                "Nome: " + nome +
+                "Preco: " + preco +
+                "Quantidade: "+ quantidade+
+                "Data de Cadastro: "+dataCadastro;
+        }
+        #endregion
+
+    }
+}
