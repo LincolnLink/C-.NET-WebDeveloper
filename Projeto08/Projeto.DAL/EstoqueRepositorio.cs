96b053aab3df4a44fd2e8b6031ea8dbe38d39928
Lincoln Ferreira Campos <link7-7@hotmail.com>
2017-08-15 00:04:08 -0300

    ok

--- Projeto08/Projeto.DAL/EstoqueRepositorio.cs
+++ Projeto08/Projeto.DAL/EstoqueRepositorio.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Projeto.DAL;
 using System.Data.SqlClient;
+using Projeto.Entidades;
 
 
 namespace Projeto.DAL
--- Projeto08/Projeto.Entidades/Produto.cs
+++ Projeto08/Projeto.Entidades/Produto.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Projeto.Entidades
 {
     public class Produto
